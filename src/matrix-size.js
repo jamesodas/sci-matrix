e34ea5568171c5aa54c717647247b887bb9ba3e2
James da Silva <jamesodas@gmail.com>
2018-02-22 18:45:59 -0300

    Definição de testes e implementação do método matrixSize

---
+++ src/matrix-size.js
@@ -0,0 +1,13 @@
+import isMatrix from './is-matrix';
+
+const matrixSize = (matriz) => {
+  if (!Array.isArray(matriz)) return undefined;
+  if (!isMatrix(matriz)) return undefined;
+  const size = new Array(2);
+  size[0] = matriz.length;
+  size[1] = matriz[0].length;
+  if (size[0] === undefined || size[1] === undefined) return undefined;
+  return size;
+};
+
+export default matrixSize;
